7c05f81c98e2d570d241ae4e223d4e66ba56eb2c
Taufiq <61982529+Uplus1D70F@users.noreply.github.com>
2022-01-25 23:19:57 +0400

    Updated styles.css and added grid to player names

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import { addPlayer } from '../actions/actions';
-
+import { addPlayer } from '../actions/actions'
 
 class Players extends Component {
   constructor(props) {
-    super(props);
-    this.handleInput = this.handleInput.bind(this);
+    super(props)
+    this.handleInput = this.handleInput.bind(this)
   }
 
   handleInput(event) {
     if (event.keyCode === 13) {
       /* enter key press */
-      this.props.addPlayer(event.target.value);
-      event.target.value = '';
+      this.props.addPlayer(event.target.value)
+      event.target.value = ''
     }
   }
 
   render() {
     let players = this.props.players.map((player, index) => {
-      return (<p className='player-text' key={index}>{player.name}</p>);
-    });
+      return (
+        <p className='player-text' key={index}>
+          {player.name}
+        </p>
+      )
+    })
     return (
       <div>
         <h2>Players</h2>
-        {players}
-        <input type='text' placeholder='Add player...' onKeyUp={this.handleInput}></input>
+        <div id='player-names-container'>
+          <div id='player-names'>{players}</div>
+        </div>
+        <input
+          type='text'
+          placeholder='Add player...'
+          onKeyUp={this.handleInput}
+        ></input>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => {
   return {
-    players: state.appReducer.players
-  };
+    players: state.appReducer.players,
+  }
 }
 
-export default connect(mapStateToProps, { addPlayer })(Players);
+export default connect(mapStateToProps, { addPlayer })(Players)
